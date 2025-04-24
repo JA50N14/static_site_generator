bdaa00cb3babeebb7f230069d92a0b8a305b6b9b
JA50N14 <macfarlane_j@hotmail.com>
2025-04-24 16:22:48 -0600

    added docs directory and basepath variable to main.py to build pages on github pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -12,7 +12,7 @@ def extract_title(markdown):
     raise Exception('There is no h1 header in the markdown file')
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f'Generating page from {from_path} to {dest_path} using {template_path}')
 
     with open(from_path, 'r') as f:
@@ -26,19 +26,21 @@ def generate_page(from_path, template_path, dest_path):
     
     html_template = html_template.replace('{{ Title }}', page_title)
     html_template = html_template.replace('{{ Content }}', html_body)
-    
+    html_template = html_template.replace('href="/', f'href="{basepath}')
+    html_template = html_template.replace('src="/', f'src="{basepath}')
+
     with open(dest_path, 'w') as f:
         f.write(html_template)
     
 
-def generate_pages_recursive(from_path, template_path, dest_path):
+def generate_pages_recursive(from_path, template_path, dest_path, basepath):
     from_path_dir_list = os.listdir(from_path)
     
     for dir_item in from_path_dir_list:
         dir_item_path = os.path.join(from_path, dir_item)
         if os.path.isdir(dir_item_path):
             os.mkdir(os.path.join(dest_path, dir_item))
-            generate_pages_recursive(dir_item_path, template_path, os.path.join(dest_path, dir_item))
+            generate_pages_recursive(dir_item_path, template_path, os.path.join(dest_path, dir_item), basepath)
         
         if dir_item.endswith('.md'):
             with open(dir_item_path, 'r') as f:
@@ -52,9 +54,9 @@ def generate_pages_recursive(from_path, template_path, dest_path):
 
             html_template = html_template.replace('{{ Title }}', page_title)
             html_template = html_template.replace('{{ Content }}', html_body)
+            html_template = html_template.replace('href="/', f'href="{basepath}')
+            html_template = html_template.replace('src="/', f'src="{basepath}')
 
-            # with open(os.path.join(dest_path, dir_item), 'w') as f:
-            #     f.write(html_template)
 
             with open(Path(dest_path, dir_item[:-3] + '.html'), 'w') as f:
                 f.write(html_template)
--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import os, shutil
-from textnode import TextType, TextNode
-from htmlnode import LeafNode
+import sys
 from copy_static import copy_static_to_public_recursive
 from generate_page import generate_page, generate_pages_recursive
 
 dir_path_content = './content'
 dir_path_template = './template.html'
 dir_path_static = './static'
-dir_path_public = './public'
-
+dir_path_public = './docs'
+basepath = sys.argv[1] if len(sys.argv) > 1 else '/'
+print(f'BASEPATH: {basepath}')
 
 def main():
     print('Deleting public directory...')
@@ -22,10 +22,11 @@ def main():
     # generate_page(
     #     os.path.join(dir_path_content, 'index.md'), 
     #     dir_path_template, 
-    #     os.path.join(dir_path_public, 'index.html')
+    #     os.path.join(dir_path_public, 'index.html'),
+    #     basepath
     # )
 
-    generate_pages_recursive(dir_path_content, dir_path_template, dir_path_public)
+    generate_pages_recursive(dir_path_content, dir_path_template, dir_path_public, basepath)
 
 main()
 
